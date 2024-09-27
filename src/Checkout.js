0ba8cb88bbdf43017ff39d44e0254f622cb19494
ibrahim <138600705+Ibrahimibu001@users.noreply.github.com>
2024-09-27 07:36:45 -0700

    Update Checkout.js

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Checkout.css';
 import Subtotal from './Subtotal';
-import { useStateValue } from "./StateProvider";
 import CheckoutProduct from "./CheckoutProduct";
 
 function Checkout() {

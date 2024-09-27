9459dce080d5472fe7d5e62d611a7d0733a8cf0a
ibrahim <138600705+Ibrahimibu001@users.noreply.github.com>
2024-09-27 08:20:20 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,10 +8,9 @@ import { StateProvider } from "./StateProvider";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App/>
     <React.StrictMode> 
     <StateProvider initialState={initialState} reducer={reducer}>
-    
+    <App/>
     </StateProvider>
     </React.StrictMode> 
 );

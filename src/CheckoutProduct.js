d5a52077f03f78a89f75b305116d8144fb2cf13d
ibrahim <138600705+Ibrahimibu001@users.noreply.github.com>
2024-09-27 07:37:05 -0700

    Update CheckoutProduct.js

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './CheckoutProduct.css';
-import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
     const [{ basket }, dispatch] = useStateValue();

7ce6a2d674057308d49943e5a5faeb002cd4145c
ibrahim <138600705+Ibrahimibu001@users.noreply.github.com>
2024-09-27 08:00:34 -0700

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,7 @@ import './Header.css';
 import { Link } from 'react-router-dom';
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+import { useStateValue } from "./StateProvider";
 
 function Header() {
   const [{ basket, user}, dispatch] = useStateValue();
